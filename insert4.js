1e4362dceaf639a7a6f49f8359bb3548d3e39e7e
Yuuta81 <104344405+Tsuchiya81@users.noreply.github.com>
2022-11-15 02:15:09 +0000

    伊藤園追加完了

---
+++ insert4.js
@@ -0,0 +1,15 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('test2.db');
+
+let sql = 
+  `insert into company ("name") values ("伊藤園");`
+
+db.serialize( () => {
+	db.run( sql, (error, row) => {
+		if(error) {
+			console.log('Error: ', error );
+			return;
+		}
+		console.log( "データを追加しました" );
+	});
+});
